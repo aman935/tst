a4620ada61b67313259c357ea8db2200347e2df4
aman935 <singh.amansingh935@gmail.com>
2015-11-01 01:58:47 +0530

    The first commit of the code The file tst.c is for tick stack toe.

---
+++ tst.c
@@ -0,0 +1,16 @@
+/* This game will be played in many modes, one of them is p1 vs. p2 and another is p1 vs comp */
+/*   The first one is easy, i am gonna take a function for that. 
+ calcwin- takes argument as dimensions of grid and returns winning length
+ There will also be a function checkwin
+ to check for a win taking */
+/* the stack as argument and the winning length.  */
+#include<stdio.h>
+
+
+
+int main()
+{
+
+
+  return 0;
+}
